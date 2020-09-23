5eed8da555087050f691de50ee35df4e31f156f5
Jay Viray <64195784+virayjj@users.noreply.github.com>
2020-09-23 10:32:18 +0100

    Create lights on and pff.py

---
+++ python/worksheets/lights on and pff.py
@@ -0,0 +1,13 @@
+import random
+done = False
+
+while not done:
+    random1 = random.randint(10, 100)
+    print("High", random1)
+    print("pause()")
+    random2 = random.randint(10, 100)
+    print("Low", random2)
+    print("pause()")
+    
+
+
