5e4bcbc2c59c69ac153189ed893ddb78a9248145
Jay Viray <64195784+virayjj@users.noreply.github.com>
2020-09-23 10:32:11 +0100

    Create carpark.py

---
+++ python/worksheets/carpark.py
@@ -0,0 +1,48 @@
+carPark = [
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0]
+]
+def parkACar():
+    global carPark
+    
+    registration = input ("Enter your car registration: ")
+    row = input ("Enter the park row: ")
+    column = input ("Enter the park column: ")
+
+    row = int(row) - 1
+    column  = int(column) - 1
+    
+    if carPark[row][column] == 0:
+        carPark[row][column] = registration
+        displayCarParkGrid()
+    else:
+        parkACar()
+
+def removeACar():
+    global carPark
+
+    registration = input ("Enter your car registration: ")
+    row = input ("Enter the park row: ")
+    column = input ("Enter the park column: ")
+
+    row = int(row) - 1
+    column  = int(column) - 1
+
+    carPark[row][column] = 0
+
+    displayCarParkGrid()
+    
+def displayCarParkGrid():
+    global carPark
+    for spaceRow in carPark:
+        print(spaceRow)
+
+
