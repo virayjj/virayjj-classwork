33cc6026e8d6e22d80ea166cb26fed32d0687120
Jay Viray <64195784+virayjj@users.noreply.github.com>
2020-11-20 13:01:36 +0000

    Update mygame(unfinished).py

--- pygame/mini-project/mygame(unfinished).py
+++ pygame/mini-project/mygame(unfinished).py
@@ -1,4 +1,25 @@
 import pygame
+
+map = [
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    ]
  
 # Define some colors
 BLACK = (0, 0, 0)
@@ -18,21 +39,63 @@ pygame.display.set_caption("My Game")
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.Surface([30,30])
-        self.image.fill(YELLOW)
+        self.image = pygame.Surface([20,20])
+        self.image.fill(WHITE)
         # Set position
         self.rect = self.image.get_rect()
-        self.rect.x = 20
-        self.rect.y = 20
+        self.rect.x = 500
+        self.rect.y = 360
         self.lives = 3
 
     def player_speed(self, x_val, y_val):
         self.rect.x += x_val
         self.rect.y += y_val
 
+    def shoot(self):
+        bullet = Bullet(self.rect.x + 5, self.rect.y - 5)
+        bullet_group.add(bullet)
+        all_sprites_list.add(bullet)
+
+class Wall(pygame.sprite.Sprite):
+    def __init__(self, color, width, height, x_ref, y_ref):
+        super().__init__()
+        self.image = pygame.Surface([width, height])
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.rect.x = x_ref
+        self.rect.y = y_ref
+        
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x_pos, y_pos):
+        super().__init__()
+        self.image = pygamge.Surface([10,10])
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.x = x_pos
+        self.rect.y = y_pos
+        self.speed = 5
+        
+
 all_sprites_list = pygame.sprite.Group()
+
 player = Player()
 all_sprites_list.add(player)
+
+wall_list = pygame.sprite.Group()
+
+bullet_group = pygame.sprite.Group()
+
+x = 0
+y = 0
+for row in map:
+    for col in row:
+        if col == 1:
+            my_wall = Wall(RED, 40, 40, x, y)
+            all_sprites_list.add(my_wall)
+            wall_list.add(my_wall)
+        x = x + 40
+    x = 0
+    y = y + 40
 
 # Loop until the user clicks the close button.
 done = False
@@ -50,24 +113,21 @@ while not done:
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
         player.player_speed(-1,0)
-        """
-        if keys[pygame.K_DOWN]:
-            player.player_speed(-1,1)
-        elif keys[pygame.K_UP]:
-            player.player_speed(-1,-1)
-        """
     elif keys[pygame.K_RIGHT]:
         player.player_speed(1,0)
-        """
-        if keys[pygame.K_DOWN]:
-            player.player_speed(1,1)
-        elif keys[pygame.K_UP]:
-            player.player_speed(1,-1)
-        """
     elif keys[pygame.K_UP]:
         player.player_speed(0,-1)
     elif keys[pygame.K_DOWN]:
         player.player_speed(0,1)
+
+    player_hit = pygame.sprite.spritecollide(player, wall_list, False)
+    for foo in player_hit:
+        player.player_speed(0,0)
+        player.rect.x = player_old_x
+        player.rect.y = player_old_y
+
+    player_old_x = player.rect.x
+    player_old_y = player.rect.y    
 
     # --- Game logic should go here
     all_sprites_list.update()
@@ -80,7 +140,7 @@ while not done:
     pygame.display.flip()
  
     # --- Limit to 60 frames per second
-    clock.tick(60)
+    clock.tick(300)
  
 # Close the window and quit.
 pygame.quit()
